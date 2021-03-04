1ae54143f9f72ca4ec12f0471da8543432ebca10
matheusfelix34 <mfelix3411@gmail.com>
2021-03-04 10:55:30 -0300

    Primeiro Commit do Cadastro de usuarios  para um sitema.

---
+++ usuarios/src/main/java/usuarios/Principal.java
@@ -0,0 +1,23 @@
+package usuarios;
+/*
+ * Cadastro de usuarios  para um sitema.
+ * Um usuario vai possuir nome, email e uma senha.
+ * A senha deve ser cripitografada.
+ * O sistema deve possuir autentificação que deve
+ * ser feita por email e senha.
+ * 
+ * Model View Controller
+ * 
+ * 
+ * 
+ * 
+ * */
+public class Principal {
+	
+	
+public static void main(String[] args) {
+	
+}
+
+
+}
